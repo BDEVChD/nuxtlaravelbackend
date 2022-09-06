83e88006addca5899e38c724ec6f1d259fd2d1f0
Bladi Duran <bladi.duran@gmail.com>
2022-09-06 15:09:48 +0100

    creates user and post tables, api endpoints, seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Post::factory(20)->create();
+
+        User::find(1)->update([
+            'name' => 'bladi',
+            'email' => 'bladi.duran@gmail.com',
+        ]);
     }
 }
